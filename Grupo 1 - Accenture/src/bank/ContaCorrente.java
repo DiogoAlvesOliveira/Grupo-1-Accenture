c2814e057d33ec296bae30a972c5f6dc790c74b1
DiogoAlvesOliveira <62778974+DiogoAlvesOliveira@users.noreply.github.com>
2021-03-25 23:00:26 -0300

    add metodos
    add metodos as class.

--- Grupo 1 - Accenture/src/bank/ContaCorrente.java
+++ Grupo 1 - Accenture/src/bank/ContaCorrente.java
@@ -3,11 +3,13 @@ package bank;
 public class ContaCorrente {
 	private int id;
 	private int contaCorrenteNumero;
+	//construtor
 	public ContaCorrente(int id, int contaCorrenteNumero) {
 		super();
 		this.id = id;
 		this.contaCorrenteNumero = contaCorrenteNumero;
 	}
+	//metodos gets e sets
 	public int getId() {
 		return id;
 	}
@@ -20,5 +22,31 @@ public class ContaCorrente {
 	public void setContaCorrenteNumero(int contaCorrenteNumero) {
 		this.contaCorrenteNumero = contaCorrenteNumero;
 	}
-	
+	// metodo para depositar
+	//falta programas o metodo
+	public void deposito(double valor) {
+		
+	}
+	//metodo para sacar
+	//falta programas o metodo
+	public void saque(double valor) {
+		
+	}
+	//metodo para transferir
+	//falta programas o metodo
+	public void trasnferencia(double valor) {
+		
+	}
+	//metodo para consultar o extrato da sua conta
+	//falta programas o metodo
+	public double extrato() {
+		double valor = 0;
+		return valor;
+	}
+	//metodo pararecalcular o extrato da sua conta
+	//falta programas o metodo
+	public double recalcularSaldo() {
+		double saldo = 0;
+		return saldo;
+	}
 }
